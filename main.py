ca20ba7897faf4f5d18dd56e134f76b7a37e82a7
nvrancovich <nvrancovich@gmail.com>
2022-06-26 12:20:13 -0300

    Add turn mechanic

--- main.py
+++ main.py
@@ -7,4 +7,22 @@ def display_board():
     print(board[3],"|",board[4],"|",board[5])
     print(board[6],"|",board[7],"|",board[8])
 
-display_board()
+def turn(player):
+    if player == 1:
+        marker = "X"
+    else:
+        marker = "O"
+    position = int(input("Selecciona una pocición del 1 al 9: ")) - 1
+    while board[position] != "-":
+        position = int(input("Esa posición ya esta ocupada, elegí otra: ")) - 1
+    board[position] = marker
+    return board
+
+def main():
+    display_board()
+    turn(1)
+    display_board()
+    turn(1)
+    display_board()
+
+main()
